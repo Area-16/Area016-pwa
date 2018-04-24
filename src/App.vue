97cca3e25614f6737d6506d44a295ac8643892a6
Douglas Alves <douglas.expalves@gmail.com>
2018-04-24 14:35:32 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,38 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+  <v-app>
+    <v-toolbar dark color="teal">
+      <span>
+        <v-toolbar-side-icon @click="sidebar = !sidebar"></v-toolbar-side-icon>
+      </span>
+      <span class="hidden-xs-and-down">
+        <v-btn class="white--text" color="purple" large depressed outline>{{ app }}</v-btn>
+      </span>
+    </v-toolbar>
+    <v-container fluid>
       <router-view></router-view>
-    </main>
-  </div>
+    </v-container>
+    <v-footer app>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      app: this.$store.state.app
+    }
+  }
 }
 </script>
 
-<style>
-body {
-  margin: 0;
-}
+<style scoped>
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
 }
 </style>
